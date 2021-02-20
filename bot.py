494d2f582385d8c31bcda09d17d23bdf7b00c89f
Michał Wyczarski <64838595+RedC4ke@users.noreply.github.com>
2021-02-20 21:30:37 +0100

    v0.0.2 - chess piece movement mostly done

--- bot.py
+++ bot.py
@@ -1,48 +1,51 @@
 import discord
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
 import chess
 
 client = discord.Client()
 conf_pref = open("prefixes.txt")
 prefixes = []
+bot = commands.Bot(command_prefix=['!', 'o!', 'outplay!'])
+bot.remove_command('help')
+game = chess.Match()
 
 
-@client.event
-async def on_ready():
-    for line in conf_pref:
-        prefixes.append(line)
-
-    print("Cyny gejm! U r {0.user}"
-          .format(client))
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    for prefix in prefixes:
-        if message.content.startswith(prefix + "help"):
-            embed = discord.Embed(title="Help page", description="Outplay is a simple Chess bot.", color=0xf4536f)
-            embed.set_thumbnail(url='https://i.imgur.com/E7JJZMv.png')
-            embed.add_field(name="`prefixes [add|remove|list]`",
-                            value="Allows you to change command prefixes. Defaults are  _!_ ,  _o!_   "
-                                  "and  _outplay!_. The bot can still be called by mentioning it.",
-                            inline=False)
-            embed.add_field(name="`challenge [user]`",
-                            value="Challenges the mentioned user to a chess duel. The invitation can be **accepted** "
-                                  "or **rejected** using reactions",
-                            inline=False)
-            embed.add_field(name="`move [notation]`",
-                            value="Makes a move. The proper notation is  `XY:XY` , where _X_  is a **letter**  "
-                                  "and _Y_  is a **number**. ",
-                            inline=False)
-            embed.add_field(name="`draw`",
-                            value="Starts a poll to **draw** the session. Draw occurs when both players agree "
-                                  "or the other player doesn't respond in **48 hours**",
-                            inline=False)
-            await message.channel.send(embed=embed)
-
-        # if message.content.startswith(prefix+"challenge")
+@bot.command()
+async def help(ctx):
+    thumbnail = discord.File('res/shield.png', 'thumbnail.png')
+    embed = discord.Embed(title="Help page", description="Outplay is a simple Chess bot.", color=0xf4536f)
+    embed.add_field(name="`prefixes [add|remove|list]`",
+                    value="Allows you to change command prefixes. Defaults are  _!_ ,  _o!_   "
+                          "and  _outplay!_. The bot can still be called by mentioning it.",
+                    inline=False)
+    embed.add_field(name="`challenge [user]`",
+                    value="Challenges the mentioned user to a chess duel. The invitation can be **accepted** "
+                          "or **rejected** using reactions",
+                    inline=False)
+    embed.add_field(name="`move [notation]`",
+                    value="Makes a move. The proper notation is  `XY:XY` , where _X_  is a **letter**  "
+                          "and _Y_  is a **number**. ",
+                    inline=False)
+    embed.add_field(name="`draw`",
+                    value="Starts a poll to **draw** the session. Draw occurs when both players agree "
+                          "or the other player doesn't respond in **48 hours**",
+                    inline=False)
+    embed.set_thumbnail(url='attachment://thumbnail.png')
+    await ctx.send(file=thumbnail, embed=embed)
 
 
-client.run('ODExOTQ0OTE1NjM4Mjg4NDA1.YC5koQ.lMRS2eiw-Mu3u0KTYeaRdzrzy6U')
+@bot.command()
+async def start(ctx):
+    game.start()
+    await ctx.send(file=discord.File(game.image()))
+
+
+@bot.command()
+async def move(ctx, arg):
+    game.move(arg, True)
+    await ctx.send(file=discord.File(game.image()))
+
+load_dotenv('keys.env')
+bot.run(os.getenv('TOKEN'))
--- chess.py
+++ chess.py
@@ -1,9 +1,56 @@
+from PIL import Image
+
+directory = 'res/chess'
+dictx = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7
+}
+dicty = {
+    '8': 0,
+    '7': 1,
+    '6': 2,
+    '5': 3,
+    '4': 4,
+    '3': 5,
+    '2': 6,
+    '1': 7
+}
+
+
 def populate(board: dict):
+    white = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
+    black = [Rook, Knight, Bishop, King, Queen, Bishop, Knight, Rook]
     for i in range(8):
         position1 = [i, 1]
         board[str(position1)] = Pawn(position1, False, board)
-        position2 = [i, 7]
+        position2 = [i, 6]
         board[str(position2)] = Pawn(position2, True, board)
+        position3 = [i, 0]
+        board[str(position3)] = black[i](position3, False, board)
+        position4 = [i, 7]
+        board[str(position4)] = white[i](position4, True, board)
+
+
+def render(board):
+    base = Image.open(directory + '/frame.png')
+    boardimg = Image.open(directory + '/board.png')
+
+    for figure in board.values():
+        if isinstance(figure, Figure):
+            pos1, pos2 = figure.position[0] * 128, figure.position[1] * 128
+            box = [pos1, pos2, pos1 + 128, pos2 + 128]
+            boardimg.paste(figure.icon, box, figure.icon)
+
+    base.paste(boardimg, (64, 64))
+    path = 'res/chess/render.png'
+    base.save(path)
+    return path
 
 
 def getpos(board: dict, position: list):
@@ -11,13 +58,19 @@ def getpos(board: dict, position: list):
 
 
 class Figure:
-    def __init__(self, pos: list, white: bool, board: dict):
-        self.movement = None
+    def __init__(self, pos: list, white: bool, board: dict, name: str, rang: int, mov: list):
+        self.movement = mov
         self.position = pos
-        self.range = None
+        self.range: int = rang
         self.white = white
         self.board = board
         self.mv = 0
+
+        if white:
+            color = '/white'
+        else:
+            color = '/black'
+        self.icon = Image.open(directory + '/pieces' + color + '/' + name + '.png').convert('RGBA')
 
     def vectormove(self, position: list, vector: list):
         if vector[0] > 0:
@@ -42,8 +95,11 @@ class Figure:
             return [1]
         else:
             vector = [directions[0] - self.position[0], directions[1] - self.position[1]]
+
             position = self.position[:]
             if (vector[0] > self.range) or (vector[1] > self.range):
+                print(directions)
+                print(vector)
                 return [2]
 
             if (vector[0] == vector[1]) and ('x' in self.movement):
@@ -56,25 +112,29 @@ class Figure:
                     if self.vectormove(position, vector) is not None:
                         return [3]
 
+            elif ('k' in self.movement) and \
+                    (vector[0] ^ vector[1] in range(-1, 2) and vector[0] ^ vector[1] in range(-2, 3)):
+                self.vectormove(position, vector)
+
             else:
                 return [4]
 
             self.vectormove(position, vector)
+            self.board[str(self.position)] = None
+            self.position = position
+            self.board[str(self.position)] = self
+            self.mv += 1
             field = getpos(self.board, position)
+
             if field is not None:
-                self.position = position
-                self.mv += 1
                 return [5, field]
             else:
-                self.position = position
-                self.mv += 1
                 return [6]
 
 
 class Pawn(Figure):
     def __init__(self, pos: list, white: bool, board: dict):
-        super(Pawn, self).__init__(pos, white, board)
-        self.movement = ['+']
+        super(Pawn, self).__init__(pos, white, board, 'pawn', 1, ['+'])
 
     def move(self, directions: list):
         if self.mv == 0:
@@ -87,10 +147,30 @@ class Pawn(Figure):
 
 class Bishop(Figure):
     def __init__(self, pos: list, white: bool, board: dict):
-        super(Bishop, self).__init__(pos, white, board)
+        super(Bishop, self).__init__(pos, white, board, 'bishop', 10, ['x'])
+
+
+class Knight(Figure):
+    def __init__(self, pos: list, white: bool, board: dict):
+        super(Knight, self).__init__(pos, white, board, 'knight', 10, ['k'])
+
+
+class Rook(Figure):
+    def __init__(self, pos: list, white: bool, board: dict):
+        super(Rook, self).__init__(pos, white, board, 'rook', 10, ['+'])
+
+
+class Queen(Figure):
+    def __init__(self, pos: list, white: bool, board: dict):
+        super(Queen, self).__init__(pos, white, board, 'queen', 10, ['x', '+'])
+
+
+class King(Figure):
+    def __init__(self, pos: list, white: bool, board: dict):
+        super(King, self).__init__(pos, white, board, 'king', 1, ['x', '+'])
 
     def move(self, directions: list):
-        super(Bishop, self).move(directions)
+        return super(King, self).move(directions)
 
 
 class Match:
@@ -99,3 +179,25 @@ class Match:
 
     def start(self):
         populate(self.board)
+
+    def image(self):
+        return render(self.board)
+
+    def move(self, arg: str, white: bool):
+        movement = arg.split(':')
+        start = [dictx.get(movement[0][0], 2137), dicty.get(movement[0][1], 420)]
+        end = [dictx.get(movement[1][0], 69), dicty.get(movement[1][1], 100)]
+        field = self.board.get(str(start))
+
+        if not isinstance(field, Figure):
+            print('huj')
+            return 0
+        elif field.white != white:
+            print('huj1')
+            return 1
+        else:
+            print(field.move(end))
+
+
+
+
